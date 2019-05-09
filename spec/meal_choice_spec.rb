b35ca8ac3fafee08e0bd922c895b915d5bf2bb0e
Justin <sumbhodi1618@gmail.com>
2019-05-09 23:43:57 +0000

    Automatically backed up by Learn

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -2,6 +2,7 @@ describe '#meal_choice' do
   it 'should default to meat' do
     expect(meal_choice).to eq(choice = "meat")
     puts "#{choice}"
+    binding.pry
   end
 
   it 'should allow you to set a meal' do
